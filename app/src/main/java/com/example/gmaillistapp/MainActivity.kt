1cf02861a2b5056fc71c0e21df723290250a3905
PhanHieu244 <phanhieu2443@gmail.com>
2024-10-28 23:27:46 +0700

    up

--- app/src/main/java/com/example/gmaillistapp/MainActivity.kt
+++ app/src/main/java/com/example/gmaillistapp/MainActivity.kt
@@ -1,45 +1,47 @@
 package com.example.gmaillistapp
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var emailAdapter: EmailAdapter
-    private val emailList = mutableListOf<Email>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
 
-        setContentView(R.layout.activity_email_list)
-        recyclerView = findViewById(R.id.recyclerView)
-        val clickListener: (Email) -> Unit = { email ->
-            println("Clicked on email from: ${email.sender}")
-        }
-        emailAdapter = EmailAdapter(emailList, clickListener)
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = emailAdapter
-
-        val addEmailLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val newEmail = result.data?.getParcelableExtra<Email>("newEmail")
-                newEmail?.let {
-                    emailList.add(it)
-                    emailAdapter.notifyItemInserted(emailList.size - 1)
-                }
-            }
-        }
-
-        // Set onClickListener for the FAB
-        val fabAddEmail = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.fabAddEmail)
-        fabAddEmail.setOnClickListener {
-            val intent = Intent(this, AddEmailActivity::class.java)
-            addEmailLauncher.launch(intent)
-        }
+        val email = arrayListOf<ItemModel>()
+        email.add(ItemModel("user1", "Edurila.com", "$19 Only (First 10 spots) - Bestselling...\n" +
+                "Are you looking to Learn Web Designin...", "12:34 PM", R.drawable.user1))
+        email.add(ItemModel("user2", "Chris Abad", "Help make Campaign Monitor better\n" +
+                "Let us know your thoughts! No Images...", "11:22 AM", R.drawable.user2))
+        email.add(ItemModel("user3", "Tuto.com", "8H de formation gratuite et les nquvea...\n" +
+                "Photoshop, SEQ, Blender, CSS, WordPre...", "11:04 AM", R.drawable.user3))
+        email.add(ItemModel("user4", "support", "Société Ovh: suivi de vó services - hp...\n" +
+                "SAS OVH - http://www.ovh.com 2 rue K...", "10:26 AM", R.drawable.user4))
+        email.add(ItemModel("user5", "Matt from Ionic", "The New Ionic Creator Is Here!\n" +
+                "Annoucing the all-new Creator, building...", "7:58 PM", R.drawable.user5))
+        email.add(ItemModel("user1", "Edurila.com", "$19 Only (First 10 spots) - Bestselling...\n" +
+                "Are you looking to Learn Web Designin...", "12:34 PM", R.drawable.user1))
+        email.add(ItemModel("user2", "Chris Abad", "Help make Campaign Monitor better\n" +
+                "Let us know your thoughts! No Images...", "11:22 AM", R.drawable.user2))
+        email.add(ItemModel("user3", "Tuto.com", "8H de formation gratuite et les nquvea...\n" +
+                "Photoshop, SEQ, Blender, CSS, WordPre...", "11:04 AM", R.drawable.user3))
+        email.add(ItemModel("user4", "support", "Société Ovh: suivi de vó services - hp...\n" +
+                "SAS OVH - http://www.ovh.com 2 rue K...", "10:26 AM", R.drawable.user4))
+        email.add(ItemModel("user5", "Matt from Ionic", "The New Ionic Creator Is Here!\n" +
+                "Annoucing the all-new Creator, building...", "7:58 PM", R.drawable.user5))
+        email.add(ItemModel("user1", "Edurila.com", "$19 Only (First 10 spots) - Bestselling...\n" +
+                "Are you looking to Learn Web Designin...", "12:34 PM", R.drawable.user1))
+        email.add(ItemModel("user2", "Chris Abad", "Help make Campaign Monitor better\n" +
+                "Let us know your thoughts! No Images...", "11:22 AM", R.drawable.user2))
+        email.add(ItemModel("user3", "Tuto.com", "8H de formation gratuite et les nquvea...\n" +
+                "Photoshop, SEQ, Blender, CSS, WordPre...", "11:04 AM", R.drawable.user3))
+        email.add(ItemModel("user4", "support", "Société Ovh: suivi de vó services - hp...\n" +
+                "SAS OVH - http://www.ovh.com 2 rue K...", "10:26 AM", R.drawable.user4))
+        email.add(ItemModel("user5", "Matt from Ionic", "The New Ionic Creator Is Here!\n" +
+                "Annoucing the all-new Creator, building...", "7:58 PM", R.drawable.user5))
+        val listView = findViewById<ListView>(R.id.listView)
+        listView.adapter = CustomEmailAdapter(email)
     }
 }
